656850d930c3af4171e5c27b1b25353ff007fe17
babamurad <babamurad2010@yandex.ru>
2024-11-13 03:03:30 +0500

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'gender' => 'required|in:male,female,other',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'gender' => $request->gender,
+        ]);
+
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        if (auth()->attempt(['email' => $request->email, 'password' => $request->password])) {
+            $token = auth()->user()->createToken('API Token')->plainTextToken;
+
+            return response()->json([
+               'message' => 'User logged in successfully',
+               'user' => auth()->user(),
+               'token' => $token,
+            ], 200);
+        }
+
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+
+    public function profile()
+    {
+        return response()->json(auth()->user());
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+
+Route::post('/registration', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/profile', [UserController::class, 'profile'])->middleware('auth:sanctum');
+
